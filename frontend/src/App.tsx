4808bbb2d94575781faa4257d588e8a84f9b1ca5
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-25 16:11:30 +0300

    init commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import logo from "/logo.svg";
+// import './App.css'
+
+function App() {
+   return (
+      <>
+         <img src={logo} width={500} className="logo react" alt="React logo" />
+      </>
+   );
+}
+
+export default App;
