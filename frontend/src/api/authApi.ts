2d784c5bcce1ff0dbb3ad9da17819ba3389a1368
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-26 23:57:18 +0300

    delete fr

--- frontend/src/api/authApi.ts
+++
@@ -1,9 +0,0 @@
-import { createApi  } from "@reduxjs/toolkit/query/react"; 
-import { baseQueryWithReAuth } from "@/api/baseQuery"; 
-
-const authApi = createApi({
-   baseQuery: baseQueryWithReAuth,
-   endpoints: () => ({}),
-});
-
-export default authApi;
--- frontend/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@/api/endpoints/auth/login.api'
-export * from '@/api/endpoints/auth/register.api'
-// export * from '@/api/endpoints/auth/logout.api'
-export * from '@/api/endpoints/auth/refresh.api' 
