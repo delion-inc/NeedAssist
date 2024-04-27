e9524d084a0165d493d6ac398443f9052cff6768
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-27 19:37:29 +0300

    fixes

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -3,27 +3,41 @@ import { CardDescription, CardFooter, CardHeader, CardTitle, Card as CardWrapper
 import { IRequest } from "@/types/request.interface";
 import convertPriority from "@/utils/convertPriority";
 import formatDate from "@/utils/formatDate";
-import InfoModal from "./InfoModal";
+import { Dialog, DialogContent, DialogTrigger } from "@/app/styles/ui/dialog";
+import { Button } from "@/app/styles/ui/button";
+// import { toggleInfoModal } from "@/app/redux/slices/modalSlice";
+// import { selectOpenInfo } from "@/app/redux/selectors";
+// import { useAppDispatch, useAppSelector } from "@/app/redux/store";
 
 const Card = ({ title, description, id, createdAt, city, user, priority }: IRequest) => {
+   // const dispatch = useAppDispatch();
+   // const open = useAppSelector(selectOpenInfo); 
+
    return (
       <>
-         <CardWrapper key={id}>
-            <CardHeader>
-               <CardTitle className="flex items-center justify-between"><span>{title}</span> <Badge className="font-light">{convertPriority(priority)}</Badge></CardTitle>
-               <CardDescription className="max-w-[750px]">
-                  {description} <br />
-                  <span className="text-muted-foreground">
-                     {user.name} {user.surname}, {city}
-                  </span>
-               </CardDescription>
-            </CardHeader> 
-            <CardFooter className="flex items-center justify-between">
-               <InfoModal />
-               {/* <Button>Допомогти</Button> */}
-               <p className="text-muted-foreground">{formatDate(createdAt)}</p>
-            </CardFooter>
-         </CardWrapper>
+         <Dialog>
+            <CardWrapper key={id}>
+               <CardHeader>
+                  <CardTitle className="flex items-center justify-between">
+                     <span>{title}</span> <Badge className="font-light">{convertPriority(priority)}</Badge>
+                  </CardTitle>
+                  <CardDescription className="max-w-[750px]">
+                     {description} <br />
+                     <span className="text-muted-foreground">
+                        {user.name} {user.surname}, {city}
+                     </span>
+                  </CardDescription>
+               </CardHeader>
+               <CardFooter className="flex items-center justify-between">
+                  <DialogTrigger asChild>
+                     <Button>Допомога</Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[500px] flex flex-col p-0">Поможи</DialogContent>
+                  {/* <Button>Допомогти</Button> */}
+                  <p className="text-muted-foreground">{formatDate(createdAt)}</p>
+               </CardFooter>
+            </CardWrapper>
+         </Dialog>
       </>
    );
 };
