b357c9b6bc3c8ad74ec5f3d69e898ae162131ced
Hrynchuk Taras <hrynchuktt@gmail.com>
2024-04-27 20:28:50 +0300

    info modal

--- frontend/src/components/InfoModal.tsx
+++ frontend/src/components/InfoModal.tsx
@@ -1,40 +1,38 @@
 import { useGetRequestQuery } from "@/api/endpoints/request.api";
 import { Badge } from "@/app/styles/ui/badge";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/app/styles/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/app/styles/ui/card";
 import { DialogContent } from "@/app/styles/ui/dialog";
 import convertPriority from "@/utils/convertPriority";
 import formatDate from "@/utils/formatDate";
-import { Loader2 } from "lucide-react";
 
 const InfoModal = ({ id }: { id: number }) => {
    const { data, isLoading } = useGetRequestQuery(id);
 
    return (
-      <>
+      <DialogContent className="sm:max-w-[500px] flex flex-col p-0">
          {isLoading ? (
-            <Loader2 color="#176B87" className="absolute top-[45%] left-[49%] h-5 w-5 animate-spin" />
+            <p className="mx-auto my-[150px] ">Завантаження...</p>
          ) : (
-            <DialogContent className="sm:max-w-[500px] flex flex-col p-0">
-               <Card key={id}>
-                  <CardHeader>
-                     <CardTitle className="flex items-center justify-between">
-                        <span>{data?.title}</span> <Badge className="font-light">{convertPriority(data?.priority || "LOW")}</Badge>
-                     </CardTitle>
-                     <CardDescription className="max-w-[750px]">
-                        {data?.description} <br />
-                        <span className="text-muted-foreground">
-                           {data?.user.name} {data?.user.surname}, {data?.city}
-                        </span>
-                     </CardDescription>
-                  </CardHeader>
-                  <CardFooter className="flex items-center justify-between">
-                     {/* <Button>Допомогти</Button> */}
-                     <p className="text-muted-foreground">{formatDate(data?.createdAt || "")}</p>
-                  </CardFooter>
-               </Card>
-            </DialogContent>
+            <Card key={id}>
+               <CardHeader >
+                  <CardTitle className="flex items-center justify-between">
+                     <span>{data?.title}</span>
+                  </CardTitle>
+                  <CardDescription className="max-w-[750px]">{data?.description}</CardDescription>
+               </CardHeader>
+               <CardContent>
+                  {data?.user.name} {data?.user.surname} <br />
+                  Місто: {data?.city} <br />
+                  Email: {data?.user.email} <br />
+                  Контактний номер телефону: {data?.user.phone}
+               </CardContent>
+               <CardFooter className="flex items-center justify-between">
+                  <p className="text-muted-foreground">{formatDate(data?.createdAt || "")}</p>
+                  <Badge className="font-light">{convertPriority(data?.priority || "LOW")}</Badge>
+               </CardFooter>
+            </Card>
          )}
-      </>
+      </DialogContent>
    );
 };
 
